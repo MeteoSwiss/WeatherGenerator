4c9349d7704543d09efc5c6e3fec7c7562042dcb
Tharmeekan Sivalingam <112775643+Tewson1@users.noreply.github.com>
2026-05-19 09:42:02 +0200

    Delete bound.py

--- bound.py
+++
@@ -1,5 +0,0 @@
-import xarray as xr
-
-ds = xr.open_zarr("/capstor/store/mch/msopr/ml/datasets/mch-realch1-fdb-1km-2005-2025-1h-pl13-v1.0.zarr", consolidated=False)
-print("lat:", float(ds["latitudes"].min()), float(ds["latitudes"].max()))
-print("lon:", float(ds["longitudes"].min()), float(ds["longitudes"].max()))
